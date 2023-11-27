ae120f9893620ade731e36c6064cf552aeedcc5f
Ritika Mishra <117935386+sidmishra-1609@users.noreply.github.com>
2023-11-27 20:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
-}
-Person.prototype.greet = function() {
-	console.log('Hello, my name is Ritika, I am 23 years old')
+  this.name = name;
+  this.age = age;
 }
 
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is Alice and I am 25 years old.`);
+};
+
 function Employee(name, age, jobTitle) {
-Person.call(this,name,age);
-this.jobTitle = jobTitle;
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is Ritika, I am 23 years old, and my job title is Manager.`);
+  console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
 }
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
